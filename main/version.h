b08574f72a888ec9576f57b1b4f4daad10caef48
Caffreyfans <Caffreyfans@163.com>
2026-04-09 22:32:53 +0800

    chore: Update version to 0.6

--- main/version.h
+++ main/version.h
@@ -1,10 +1,10 @@
 /*
  * @Author: Caffreyfans
  * @Date: 2021-06-26 20:40:32
- * @LastEditTime: 2021-06-26 20:41:43
+ * @LastEditTime: 2024-04-09 21:45:00
  * @Description: 
  */
 #ifndef VERSION_H_
 #define VERSION_H_
-#define CORE_VERSION "0.1"
+#define CORE_VERSION "0.6"
 #endif // VERSION_H_
